8ae005005defdc780e34c385ce491123c48864f1
there-elle <maynigojanelle@gmail.com>
2022-09-14 15:24:30 +0800

    lab

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/there-elle/CIT142G.git
+git remote set-url origin https://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@github.com/there-elle/CIT142G.git
 pause
 git add .
 git commit -m "Auto Upload"
